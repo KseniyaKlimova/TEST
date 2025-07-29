270cbe2700762561405c225e4349fea8f8e9739d
Kseniya_Klimova <ksenyaru3112@gmail.com>
2025-07-29 17:27:35 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 13_676;
+        int ticket = 15_00;
         int mile = 20;
         int bonus = ticket / mile;
-        int sale = bonus * 10;
+        int sale = bonus * 10; // пример пример
 
         System.out.println("Начислено миль: " + bonus);
         System.out.println("Скидка: " + sale);
