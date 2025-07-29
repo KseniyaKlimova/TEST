4ff8e77bfac63658b023fffdb97bc2948726e212
Kseniya_Klimova <ksenyaru3112@gmail.com>
2025-07-29 17:24:58 +0300

    Changes Main Second commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
         int ticket = 13_676;
         int mile = 20;
         int bonus = ticket / mile;
+        int sale = bonus * 10;
 
         System.out.println("Начислено миль: " + bonus);
+        System.out.println("Скидка: " + sale);
     }
 }
