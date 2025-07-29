4340419da91e28440879547123d683d5ceaf48fa
Kseniya_Klimova <ksenyaru3112@gmail.com>
2025-07-29 17:14:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 13_676;
+        int mile = 20;
+        int bonus = ticket / mile;
+
+        System.out.println("Начислено миль: " + bonus);
+    }
+}
